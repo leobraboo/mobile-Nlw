5d99642f9ec7a13d1222baa79de29469c3b53974
leobraboo <leonardo.l.souza@ufv.br>
2022-09-18 15:53:40 -0300

    configuração inicial A4

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -2,11 +2,15 @@ import { TouchableOpacity, TouchableOpacityProps  , ImageBackground, ImageSource
 import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from './styles';
 import { THEME } from '../../theme';
+
+
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number; 
+    };
+    bannerUrl: string;
 }
 
 interface Props {
@@ -23,7 +27,7 @@ export function GameCard({ data, ...rest }: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
         <ImageBackground
             style={styles.cover}
-            source={data.cover}
+            source={{ uri: data.bannerUrl}}
         >
 
         <LinearGradient 
@@ -31,10 +35,10 @@ export function GameCard({ data, ...rest }: Props) {
             style={styles.footer}
         >
             <Text style={styles.name}>
-                {data.name}
+                {data.title}
             </Text>
             <Text style={styles.ads}>
-                {data.ads} anúncios
+                {data._count.ads} anúncios
             </Text>
 
         </LinearGradient>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,22 @@
+import { useEffect , useState } from 'react';
+
 import { View , Image, FlatList } from 'react-native';
-import { GameCard } from '../../components/GameCard';
+import { GameCard , GameCardProps } from '../../components/GameCard';
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading';
-
-import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
 export function Home() {
+
+  const [games , setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.2.113:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image
@@ -21,7 +30,7 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({item}) =>(
           <GameCard 
